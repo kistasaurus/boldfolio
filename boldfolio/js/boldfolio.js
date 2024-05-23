c0d5d871ef679f1475f301a5bde34d89d532fee6
Kristin <3922004+kistasaurus@users.noreply.github.com>
2024-05-23 16:04:25 -0500

    Initial Boldfolio files commit

---
+++ boldfolio/js/boldfolio.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * Boldfolio behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.boldfolio = {
+    attach (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
