cf617edb28f253263cb5362c1a5944867339670f
Kristin <3922004+kistasaurus@users.noreply.github.com>
2024-09-24 14:02:37 -0500

    Additional initial style and configuration updates

--- js/boldfolio.js
+++ js/boldfolio.js
@@ -8,14 +8,6 @@
 
   Drupal.behaviors.boldfolio = {
     attach (context, settings) {
-      $("body").prepend("<div class='theme-switcher'>Click Here</div>");
-      $(".theme-switcher").on("click", function() {
-        $("html").css("--color-dark", "var(--seafoam-dark)");
-        $("html").css("--color-highlight", "var(--seafoam-highlight");
-        $("html").css("--color-light", "var(--seafoam-light");
-        $("html").css("--color-background", "var(--seafoam-background");
-        $("html").css("--color-alternate", "var(--seafoam-alternate");
-      });
     }
   };
 
