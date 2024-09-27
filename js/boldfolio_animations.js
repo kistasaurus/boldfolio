27665b5fc0cd2a0a18470fcb84b31b14861990b9
Kristin <3922004+kistasaurus@users.noreply.github.com>
2024-09-27 16:46:41 -0500

    Additional styles and set up

--- js/boldfolio_animations.js
+++ js/boldfolio_animations.js
@@ -2,22 +2,35 @@
 	Drupal.behaviors.boldfolioAnimations = {
 		attach: function (context, settings) {
 			once('boldfolioAnimations', 'html', context).forEach(function (element) {
-				// Apply the myCustomBehaviour effect to the elements only once.
-				$('.boldfolio-teaser').waypoint(function (direction) {
-					if (direction === 'down') {
-						$(this.element).toggleClass('animate__animated animate__fadeIn');
-					}
-				}, {
-					offset: '100%'
-				})
+				// Function to check if an item is in the viewport
+				$.fn.isInViewport = function () {
+					var elementTop = $(this).offset().top;
+					var elementBottom = elementTop + $(this).outerHeight();
 
-				$('.boldfolio-animate-fadeIn').waypoint(function (direction) {
-					if (direction === 'down') {
-						$(this.element).toggleClass('animate__animated animate__fadeIn');
+					var viewportTop = $(window).scrollTop();
+					var viewportBottom = viewportTop + $(window).height();
+
+					return elementBottom > viewportTop && elementTop < viewportBottom;
+				};
+
+				// Let the teasers fade in if they haven't been viewed yet
+				$('.boldfolio-teaser').each(function (i) {
+					if (!$(this).isInViewport()) {
+						$(this).css('opacity', '0');
+					} else {
+						$(this).addClass('boldfolio-animation-done');
 					}
-				}, {
-					offset: '100%'
-				})
+				});
+
+				$(window).on('resize scroll', function () {
+					$('.boldfolio-teaser').each(function (i) {
+						if ($(this).isInViewport() && !$(this).hasClass('boldfolio-animation-done')) {
+							$(this).addClass('animate__animated animate__fadeIn animate__slow');
+							$(this).addClass('boldfolio-animation-done');
+							$(this).css('opacity', 'unset');
+						}
+					});
+				});
 		});
 	},
 };
